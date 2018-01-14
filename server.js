2a32446256c25897334e18b18f7db562364e22c9
Shawn Wilkinson <me@super3.org>
2018-01-13 20:44:49 -0500

    Change Ignore

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/',function(req,res){
 });
 
 // start the server, if running this script alone
-/* istanbul ignore next */
+/* istanbul ignore if  */
 if (require.main === module) {
   app.listen(port, function() {
     console.log('Server started! At http://localhost:' + port);
