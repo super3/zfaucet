24f6b02e4d14969ae781b04c88ddb0dcbf170ef3
Shawn Wilkinson <me@super3.org>
2018-01-19 12:17:44 -0500

    Clean Up

--- server.js
+++ server.js
@@ -2,20 +2,20 @@
 var express = require('express');
 var path    = require("path");
 var app     = express();
+
 var port    = process.env.PORT || 5000;
-var db = require('./db.js');
+var db      = require('./db.js');
 
 // make the css folder viewable
 app.use(express.static('public/css'));
 
-// create application/json parser
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); // create application/json parser
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // index route
 app.get('/',function(req, res){
-  db.latestDrips();
+  db.latestDrips(); // TODO: Figure out how to insert into template.
   res.sendFile(path.join(__dirname + '/public/index.html'));
   //__dirname : It will resolve to your project folder.
 });
@@ -23,9 +23,9 @@ app.get('/',function(req, res){
 // add route
 app.post('/api/add', function (req, res) {
   if (!req.body) return res.sendStatus(400);
+
   db.createDrip(req.body.inputAddress);
-  console.log("Inserted: " + req.body.inputAddress);
-  res.redirect('/');
+  res.redirect('/'); // TODO: Figure out how to use AJAX, and remove this.
 });
 
 // start the server, if running this script alone
