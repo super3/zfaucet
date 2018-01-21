d06d766de1b7b97f510b2cba899f9d7593540341
Shawn Wilkinson <me@super3.org>
2018-01-20 19:48:42 -0500

    More Coverage

--- test/utils.unit.js
+++ test/utils.unit.js
@@ -6,16 +6,29 @@ describe('Backend Utils', function() {
   describe('timeSince function', function() {
 
     // subtract hours from a Date
-    Date.prototype.subtractHours= function(h){
-      this.setHours(this.getHours()-h);
+    Date.prototype.subtractMinutes= function(m){
+      this.setMinutes(this.getMinutes()-m);
       return this;
     };
 
     it('check timeSince function', function() {
-      // current date plus 2 hours
       var currentTime = new Date();
-      currentTime.subtractHours(2);
+
+      // current time
+      expect(utils.timeSince(currentTime)).to.equal('0 seconds');
+
+      // subtract 10 minutes
+      currentTime.subtractMinutes(10);
+      expect(utils.timeSince(currentTime)).to.equal('10 minutes');
+
+      // subtract 2 hours
+      currentTime.subtractMinutes(120);
       expect(utils.timeSince(currentTime)).to.equal('2 hours');
+
+      // subtract 2 days
+      currentTime.subtractMinutes(2880);
+      expect(utils.timeSince(currentTime)).to.equal('2 days');
+
     });
 
   });
