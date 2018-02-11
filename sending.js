d562d3568af3f9073c87dc7117c28c626eddd0ed
Shawn Wilkinson <me@super3.org>
2018-02-11 14:47:58 -0500

    Output Correct

--- sending.js
+++ sending.js
@@ -7,22 +7,25 @@ var config = require('./config.js');
 
 // $ ./src/zcash-cli z_sendmany "$ZADDR" "[{\"amount\": 0.8, \"address\": \"$FRIEND\"}]"
 
+// Check for zcash install
+// if (!shell.which('zcash-cli')) {
+//   shell.echo('Sorry, this script requires zcash-cli');
+//   shell.exit(1);
+// }
+
 r.connect(config.connectionConfig, function(err, conn) {
   if(err) throw err;
 
   db.pendingDrips(conn).then(function(cursor) {
     cursor.toArray(function(err, rows) {
-      console.log(rows);
+      var command = 'zcash-cli z_sendmany "' + config.sendingAddress +
+       '" [{\"amount\": ' + config.sendingAmount + ', \"address\": \"' +
+        rows[0].payoutAddress + '\"}]"';
+       console.log(command);
     });
   });
 
 });
-
-// Check for zcash install
-if (!shell.which('zcash-cli')) {
-  shell.echo('Sorry, this script requires zcash-cli');
-  shell.exit(1);
-}
 
 // // Run external tool synchronously
 // if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
