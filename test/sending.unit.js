1bd4275da20a09899c0b13ab91c03a34602c8e75
Shawn Wilkinson <me@super3.org>
2018-02-18 12:40:53 -0500

    Fix Tests

--- test/sending.unit.js
+++ test/sending.unit.js
@@ -5,11 +5,11 @@ describe('Sending Script', function() {
 
   it('create command function', function() {
     var expOut = `zcash-cli z_sendmany "t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7" `;
-    expOut += `"[{\\"amount\\": 0.0001,\\"address\\": `;
-    expOut += `\\"t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7\\"}]"`;
+    expOut += `"[{\\"amount\\": 0.00001,\\"address\\": `;
+    expOut += `\\"t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7\\"}]" 1 0.00001`;
 
     expect(sending.createCmd('t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7',
-       0.0001, 't1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7')).to.equal(expOut);
+       0.00001, 't1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7')).to.equal(expOut);
   });
 
 });
