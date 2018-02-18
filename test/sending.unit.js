971aed21b3062678e66e6f663140324cbf39a38e
Shawn Wilkinson <me@super3.org>
2018-02-17 19:26:44 -0500

    Add Test

--- test/sending.unit.js
+++ test/sending.unit.js
@@ -1,2 +1,15 @@
 var expect = require('chai').expect;
 var sending = require('../sending.js');
+
+describe('Sending Script', function() {
+
+  it('create command function', function() {
+    var expOut = `zcash-cli z_sendmany "t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7" `;
+    expOut += `"[{\\"amount\\": 0.0001,\\"address\\": `;
+    expOut += `\\"t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7\\"}]"`;
+
+    expect(sending.createCmd('t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7',
+       0.0001, 't1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7')).to.equal(expOut);
+  });
+
+});
