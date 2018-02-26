c2c14e3ea0a360f591c76a27870719999c9bb8d1
Shawn Wilkinson <me@super3.org>
2018-02-26 17:14:32 -0500

    Commit Current

--- sending2.js
+++ sending2.js
@@ -33,6 +33,13 @@ function indexOfMax(arr) {
     return maxIndex;
 }
 
+async function testBalance() {
+  var info = await rpc.getinfo();
+  return info.balance;
+}
+
+module.exports.testBalance = testBalance;
+
 async function findInputs(conn) {
   var info = await rpc.getinfo();
   console.log(`Current Balance: ${info.balance}`);
--- test/sending.unit.js
+++ test/sending.unit.js
@@ -1,16 +1,12 @@
 var expect = require('chai').expect;
-var sending = require('../sending.js');
+var sending = require('../sending2.js');
 
 describe('Sending Script', function() {
 
-  it('create command function', function() {
-    var expOut = `zcash-cli z_sendmany "t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7" `;
-    expOut += `"[{\\"amount\\": 0.000001,\\"address\\": `;
-    expOut += `\\"t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7\\"}]" 1 0.000001`;
-
-    expect(sending.createCmd('t1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7',
-       0.000001, 't1gLDWRtoY8xCyyMwQWoUCkm8fjiUJooUY7', 0.000001)).
-       to.equal(expOut);
+  it('test balance', function() {
+    sending.testBalance().then(balance => {
+      console.log(balance);
+    });
   });
 
 });
