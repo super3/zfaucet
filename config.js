1c46433f73910af8a9b7e23d7fb1e840d3f8e333
Shawn Wilkinson <me@super3.org>
2018-02-27 10:33:11 -0500

    Load From ENV

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   connectionConfig: { host: 'localhost', port: 28015 },
   sendingAmount: 0.000001,
   sendingFee: 0.000001,
-  hashes: 1024,
+  hashes: process.env.HASHES || 256,
   port: process.env.PORT || 80,
   rpcuser: process.env.rpcuser,
   rpcpass: process.env.rpcpass,
