7767ec558b36498ce9999bace06a98253ae51fb8
Shawn Wilkinson <me@super3.org>
2018-02-27 15:17:53 -0500

    Fix Tests

--- test/testHelper.js
+++ test/testHelper.js
@@ -1,6 +1,6 @@
 function validateCaptcha(captchaToken) {
   if (captchaToken !== 'DS6WL3kCBmnMSPN3vsXspJEOdEIP6Era') {
-    return Promise.reject(JSON.stringify({
+    return Promise.resolve(JSON.stringify({
       "success": false,
     }));
   } else {
