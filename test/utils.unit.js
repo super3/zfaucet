b4bb0fec7bb5d6f533a9e7e60a6a7d35f27a1794
Shawn Wilkinson <me@super3.org>
2018-02-27 09:54:18 -0500

    Better Coverage

--- test/utils.unit.js
+++ test/utils.unit.js
@@ -42,7 +42,8 @@ describe('Backend Utils', function() {
         expect(utils.isAddress('notvalidaddress')).to.equal(false);
       });
       it('wrong length', function () {
-        expect(utils.isAddress('t1Zo4ZtTpu7tvdXvZRBZvC')).to.equal(false);
+        expect(utils.isAddress('t1Zo4ZtTpu7tvdXvZRBZvC')).
+        to.equal(false);
       });
       it('changed address', function () {
         let changedAddress = 't1KjU2TUgNuWmbyEmYh19zJL5iiF5XdUsoa';
@@ -53,6 +54,39 @@ describe('Backend Utils', function() {
         expect(utils.isAddress(bitcoinAddress)).to.equal(false);
       });
 
+  });
+
+  describe('indexOfMax function', function () {
+      it('empty array', function () {
+          var arr = [];
+          expect(utils.indexOfMax(arr)).to.equal(-1);
+      });
+
+      it('sample inputs', function () {
+        var sample = [
+            {
+              "txid": "8f0a16f24fb8493f22f37ef960ca14cc6c9c3c02f5d2531739776bf5b4888d65",
+              "vout": 1,
+              "generated": false,
+              "address": "t1atPPxpdgpzC7TUNtZLMq7KCUieEYuJKkn",
+              "scriptPubKey": "76a914baa0073177890860e854780b0db792333f79df1388ac",
+              "amount": 0.00196000,
+              "confirmations": 340,
+              "spendable": true
+            },
+            {
+              "txid": "80e2185b6b12b77dbc11bf6105b7cb801d3e44eb65fed6858a592f2781a5afb6",
+              "vout": 1,
+              "generated": false,
+              "address": "t1atPPxpdgpzC7TUNtZLMq7KCUieEYuJKkn",
+              "scriptPubKey": "76a914baa0073177890860e854780b0db792333f79df1388ac",
+              "amount": 0.00197900,
+              "confirmations": 390,
+              "spendable": true
+            }
+          ];
+          expect(utils.indexOfMax(sample)).to.equal(1);
+      });
   });
 
 });
