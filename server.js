85cda5ae95825b4978d112f50cb6fba680770ea2
Shawn Wilkinson <me@super3.org>
2018-03-03 14:49:06 -0500

    Remove Faucet Page

--- server.js
+++ server.js
@@ -28,8 +28,7 @@ app.get('/', async (req, res) => {
 	const conn = await r.connect(config.connectionConfig);
 
 	// pass drips to ejs for rendering
-	const cursor = await db.latestDrips(conn);
-	const rows = await cursor.toArray();
+	const rows = await db.latestDrips(conn);
 
 	// make time in rows human readable, and then send to template
 	res.render('index', {drips: utils.readableTime(rows), hashes:
@@ -56,39 +55,6 @@ app.get('/api/withdraw/:address', async (req, res) => {
 	await db.createDrip(req.params.address);
 
 	res.end('true');
-});
-
-// index route
-app.get('/faucet', async (req, res) => {
-	const conn = await r.connect(config.connectionConfig);
-
-	// pass drips to ejs for rendering
-	const cursor = await db.latestDrips(conn);
-	const rows = await cursor.toArray();
-
-	// make time in rows human readable, and then send to template
-	res.render('faucet', {drips: utils.readableTime(rows), hashes:
-	config.hashes, pubKey: config.coinhivePubKey});
-});
-
-// add route
-app.post('/api/add', async (req, res) => {
-	// empty input, valid zcash address, then empty captcha
-	if (!req.body.inputAddress)	return res.sendStatus(400);
-	if (!utils.isAddress(req.body.inputAddress)) return res.sendStatus(400);
-	if (!req.body['coinhive-captcha-token']) return res.sendStatus(400);
-
-	// check if captcha is valid
-	const response = await coinhive.validateCaptcha(
-	req.body['coinhive-captcha-token']);
-
-	// check success response
-	if (JSON.parse(response).success === false)
-		return res.sendStatus(400);
-
-	// save to db, and redirect to index
-	await db.createDrip(req.body.inputAddress);
-	res.redirect('/faucet');
 });
 
 /* istanbul ignore next */
