d39c37c871d14495a6c629d07ab37d353f34ba89
Shawn Wilkinson <me@super3.org>
2018-03-04 15:54:30 -0500

    Remove Uneeded Config

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ module.exports = {
 	connectionConfig: {host: 'localhost', port: 28015},
 	sendingAmount: 0.000001,
 	sendingFee: 0.000001,
-	hashes: process.env.HASHES || 256,
 	port: process.env.PORT || 80,
 	rpcuser: process.env.rpcuser,
 	rpcpass: process.env.rpcpass,
