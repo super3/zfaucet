53c25655ba4679ab610fc7362850c72fbeac660f
Shawn Wilkinson <me@super3.org>
2018-03-09 16:36:40 -0500

    Use Vue Components

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,30 @@ async function get(url) {
 	return data;
 }
 
+const TransactionsTable = Vue.component('transactions-table', {
+	props: ['drips'],
+	template: `
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th scope="col">Time</th>
+					<th scope="col">Address</th>
+					<th scope="col">Transaction ID</th>
+				</tr>
+			</thead>
+			<tbody>
+					<tr v-for="drip in drips">
+						<td>{{drip.timestamp}}</td>
+						<td><a v-bind:href="'https://explorer.zcha.in/accounts/' + drip.payoutAddress">{{drip.payoutAddress}}</a></td>
+						<td v-if="drip.processed === false">Pending...</td>
+						<td v-else-if="drip.processed === true && !drip.transactionId">Sent.</td>
+						<td v-else><a v-bind:href="'https://zcash.blockexplorer.com/tx/' + drip.transactionId">View Transaction</a></td>
+					</tr>
+			</tbody>
+		</table>
+	`
+});
+
 let engine;
 
 const app = new Vue({
@@ -80,6 +104,9 @@ const app = new Vue({
 	},
 	async created() {
 		await this.validateAddress();
+	},
+	components: {
+		TransactionsTable
 	}
 });
 
