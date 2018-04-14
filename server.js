0237f6c532e8855a3f60f4053ff6b3efb3691ec6
Shawn Wilkinson <me@super3.org>
2018-04-14 11:01:14 -0400

    Small Cleanup
    - Add Favicon
    - Updated Node Version
    - Removed Unused Dep
    - Cleanup HTML

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const cache = apicache.middleware;
 // make the css folder viewable
 app.use(express.static('public/css'));
 app.use(express.static('public/js'));
+app.use(express.static('public/assets'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
