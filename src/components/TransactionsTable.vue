794124c070105526b0e81dc9f6453aea9b955c44
Monty Anderson <montydev@protonmail.com>
2018-04-30 15:29:50 +0100

    Brutal Refactor

---
+++ src/components/TransactionsTable.vue
@@ -0,0 +1,29 @@
+<template>
+	<table class="table">
+		<thead>
+			<tr>
+				<th scope="col">Time</th>
+				<th scope="col">Address</th>
+				<th scope="col">Transaction ID</th>
+				<th scope="col">Referral?</th>
+			</tr>
+		</thead>
+		<tbody>
+				<tr v-for="drip in drips">
+					<td>{{drip.timestamp}}</td>
+					<td><a v-bind:href="'https://explorer.zcha.in/accounts/' + drip.payoutAddress">{{drip.payoutAddress}}</a></td>
+					<td v-if="drip.processed === false">Pending...</td>
+					<td v-else-if="drip.processed !== false && !drip.transactionId">Sent.</td>
+					<td v-else><a v-bind:href="'https://zcash.blockexplorer.com/tx/' + drip.transactionId">View Transaction</a></td>
+					<td v-if="drip.referralAddress !== ''">Yes</td>
+					<td v-else>No</td>
+				</tr>
+		</tbody>
+	</table>
+</template>
+
+<script>
+module.exports = {
+	props: ['drips']
+};
+</script>
---
+++ src/socket.js
@@ -0,0 +1,5 @@
+const io = require('socket.io-client');
+
+const dev = window.location.href === 'http://localhost/';
+module.exports = io.connect(dev ? 'http://localhost:3012' :
+	'http://zfaucet.org:3012');
