3c9a6c943de441b9140e57f658c5d7ef3d2f7ab4
Monty Anderson <montydev@protonmail.com>
2018-05-01 10:52:31 +0100

    Fix Linter Errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-/* global localStorage, withdrawThreshold, referralAddress, window */
 const Vue = require('vue/dist/vue.common');
-const axios = require('axios');
-const Engine = require('./engine');
-const socket = require('./socket');
-
 const App = require('./components/App.vue');
 
-const app = new Vue({
+new Vue({
 	el: '#app',
 	components: {
 		App
